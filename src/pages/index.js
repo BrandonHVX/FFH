855fb9ed947681d3d7a6beeade07c584332797d4
Brandon <brandongines@gmail.com>
2019-11-14 14:29:56 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,98 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Scroller from "../components/scroller"
+import PortfolioModal from "../components/portfolio/modal"
+import PortfolioCarousel from "../components/portfolio/carousel"
+
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    Scroller.handleAnchorScroll = Scroller.handleAnchorScroll.bind(this)
+    this.state = {
+      modalShow: false,
+      modalCurrent: 0,
+    }
+    this.handlePortfolioClick = this.handlePortfolioClick.bind(this)
+    this.setModal = this.setModal.bind(this)
+  }
+
+  handlePortfolioClick(index, e) {
+    e.preventDefault()
+    this.setModal(true, index)
+  }
+
+  setModal(isShown, current) {
+    this.setState({
+      modalShow: isShown,
+      modalCurrent: current,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+
+        <section className="page-section" id="contact">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="mt-0">Let's Get In Touch!</h2>
+                <hr className="divider my-4" />
+                <p className="text-muted mb-5">
+                  Ready to start your next project with us? Give us a call or
+                  send us an email and we will get back to you as soon as
+                  possible!
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
+                <div>+1 (305) 555-0149</div>
+              </div>
+              <div className="col-lg-4 mr-auto text-center">
+                <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                <a className="d-block" href="mailto:contact@yourwebsite.com">
+                  floridafilmhouse@gmail.com
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <PortfolioModal
+          show={this.state.modalShow}
+          onHide={() => this.setModal(false, 0)}
+        >
+          <PortfolioCarousel
+            images={this.props.data.images.edges}
+            current={this.state.modalCurrent}
+          />
+        </PortfolioModal>
+      </Layout>
+    )
+  }
+}
+
+export const imageData = graphql`
+  query {
+    images: allFile(
+      filter: { relativePath: { glob: "portfolio/fullsize/*.jpg" } }
+      sort: { fields: name }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
